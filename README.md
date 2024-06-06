22b9275e2ab12fc9e19f647bba6f0b9c5dc74199
oKelvinCosta <okelvincosta@gmail.com>
2024-06-05 23:37:35 -0300

    💄 feat: Interações

---
+++ README.md
@@ -0,0 +1,6 @@
+# Teste FIEMG utilizando HTML, CSS, Javascript e Bootstrap
+
+## Design das Telas
+```bash
+https://www.figma.com/design/sYXUA4aHmBCTcddbZEi2tt/Teste-t%C3%A9cnico-web-designer?node-id=2-2&t=tBWN0OTUlEglnBna-0
+```
--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+
+// Aguardar o carregamento do DOM
+document.addEventListener("DOMContentLoaded", function () {
+  
+
+// Funcionalidade Campo Password Modal ----------------------------------------------------
+
 // Seleciona o Ícone da senha pelo ID
 const togglePassword = document.querySelector("#togglePassword");
 // Seleciona o Input de Senha pelo ID
@@ -19,5 +26,44 @@ togglePassword.addEventListener("click", () => {
   } else {
     togglePassword.classList.add("bi-eye");
     togglePassword.classList.remove("bi-eye-slash");
+  }
+});
+
+
+// Funcionalidade Video ----------------------------------------------------
+
+  // Seleciona os itens
+  var iframe = document.querySelector("#sobreVideo");
+  var vimeo = document.querySelector("#vimeo");
+  var playBtn = document.querySelector("#playBtn");
+  var imgVideo = document.querySelector("#imgVideo");
+
+  // Cria uma instância do player
+  var player = new Vimeo.Player(iframe);
+
+  // Função para dar play no vídeo
+  function playVideo() {
+    player
+      .play()
+      .then(function () {
+        // O vídeo está tocando
+
+        // Remove a imagem e botão e insere o video do vimeo
+        playBtn.classList.add("d-none");
+        imgVideo.classList.add("d-none");
+        vimeo.classList.remove("d-none");
+
+        console.log("Vídeo está tocando");
+      })
+      .catch(function (error) {
+        // Um erro ocorreu
+        console.error("Erro ao tentar tocar o vídeo:", error);
+      });
+  }
+
+  // Adiciona um evento de clique ao botão de play
+  var playButton = document.querySelector("#playBtn");
+  if (playButton) {
+    playButton.addEventListener("click", playVideo);
   }
 });
